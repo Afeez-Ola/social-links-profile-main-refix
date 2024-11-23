dbb28ec48273b46bf189ce38670efb39f7761b46
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-23 16:48:48 +0100

    setting up tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,18 @@ module.exports = {
     container: {
       center: true
     },
-    fontFamily: {
-      'display':["Inter", sans-serif],
-      'body':["Inter", sans-serif]
+    colors: {
+      grey: {
+        light: "#333333",
+        dark: "#1F1F1F",
+        darkest: "#141414"
+      },
+      green:{
+        DEFAULT: "#C4F82A"
+      },
+      white:{
+        DEFAULT: "#FFFFFF"
+      }
     }
   },
   plugins: [],
