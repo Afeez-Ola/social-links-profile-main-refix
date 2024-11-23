d5fc8ddf4774a626affdd95e3242d661bdb717f6
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-23 16:30:56 +0100

    setting up tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./assets", "./index.html", "./css"],
   theme: {
     extend: {},
+    container: {
+      center: true
+    },
+    fontFamily: {
+      'display':["Inter", sans-serif],
+      'body':["Inter", sans-serif]
+    }
   },
   plugins: [],
 }
